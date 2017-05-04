835263dbae51602fca4d92d6f92d291fe5e9ad44
Ben Sander <ben.sander@amd.com>
2017-05-04 21:35:14 +0000

    Add script to sum results.

--- sumtests.sh
+++ sumtests.sh
@@ -1,12 +1,13 @@
-
 #!/bin/bash 
 # sumtests.sh [test_output_file]
 
-sed 's/.* //g' $1 > $1.filt
+sed 's/^.*//g' $1 > $1.filt
 
-echo -n PASS,  && grep PASS $1.filt | wc -l
+PASS=$(grep PASS $1.filt | wc -l)
+TOTAL=$(grep '\.\.\.\.\.\.\.\.\.\.\.\.' $1.filt | wc -l)
+echo PASS, $PASS
 echo -n ERROR,  && grep ERROR $1.filt | wc -l
 echo -n FAIL,  && grep FAIL $1.filt | wc -l
 echo -n WAIT,  && grep WAIT $1.filt | wc -l
-echo -n FAULT,  && grep fault $1.filt | wc -l
-echo -n Total, && grep '\.\.\.\.\.\.\.\.\.\.\.\.' $1.filt | wc -l
+echo  Total,  $TOTAL
+echo -n "PassRate, "  && echo "scale=2;$PASS * 100.0 / $TOTAL" | bc
